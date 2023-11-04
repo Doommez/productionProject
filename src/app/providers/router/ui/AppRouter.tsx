e726d09849cd382df7a828f7598f12d55c341ba1
e.boganov <jenyaz862@gmail.com>
2023-11-04 18:08:34 +0300

    finish 11 stage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -7,7 +7,7 @@ export const AppRouter = () => {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
-          <Route path={path} element={element} />
+          <Route path={path} key={path} element={element} />
         ))}
       </Routes>
     </Suspense>
