8ffe96913edf5949e3dcb37d24a954ccc491c34c
E. Boganov <jenyaz862@gmail.com>
2025-07-16 15:23:11 +0300

    feat: add jest tests, finish 20 step

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', "i18next"],
-  rules: {
-    'react/jsx-indent': [0, 2],
-    'react/jsx-indent-props': [0, 2],
-    indent: [0, 2],
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.tsx'] },
-    ],
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'no-unused-vars': 'warn',
-    'react/require-default-props': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'warn',
-    'react/function-component-definition': 'off',
-    'no-shadow': 'off',
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-underscore-dangle': 'off',
-    quotes: ['error', 'single'],
-    "linebreak-style": 0,
-    'comma-dangle': ['error', 'never'],
-    'i18next/no-literal-string': ['error', {markupOnly: true}]
-  },
-  globals: {
-    __IS_DEV: true,
-  },
+    extends: ['plugin:react/recommended', 'airbnb'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint', 'i18next'],
+    rules: {
+        'react/jsx-indent': [0, 2],
+        'react/jsx-indent-props': [0, 2],
+        indent: [0, 2],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] }
+        ],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        quotes: ['error', 'single'],
+        'linebreak-style': 0,
+        'comma-dangle': ['error', 'never'],
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', {
+            ignoreComments: true
+        }]
+    },
+    globals: {
+        __IS_DEV: true
+    }
 };
 
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -8,8 +8,8 @@ export function classNames(
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
-      .filter(([cls, value]) => Boolean(value))
+      ...Object.entries(mods)
+      .filter(([_, value]) => Boolean(value))
       .map(([cls]) => cls)
   ].join(' ');
 }
