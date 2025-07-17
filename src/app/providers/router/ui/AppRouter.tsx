52bbc3e061b103d5a3d742f352ff90ae00e0b931
E. Boganov <jenyaz862@gmail.com>
2025-07-17 10:53:23 +0300

    feat: add loader and loader page finish 21

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,25 @@
-import { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
-export const AppRouter = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            path={path}
-            key={path}
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <div className="page-wrapper">{element}</div>
-              </Suspense>
-            }
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
-};
+export const AppRouter = () => (
+
+    <Routes>
+        {Object.values(routeConfig)
+            .map(({
+                      element,
+                      path
+                  }) => (
+                <Route
+                    path={path}
+                    key={path}
+                    element={(
+                        <Suspense fallback={<PageLoader />}>
+                            <div className="page-wrapper">{element}</div>
+                        </Suspense>
+                    )}
+                />
+            ))}
+    </Routes>
+);
