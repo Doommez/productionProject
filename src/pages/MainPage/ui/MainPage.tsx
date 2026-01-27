45eeabb5ee89d71c7404af41e8f96875df831993
E. Boganov <jenyaz862@gmail.com>
2026-01-27 16:39:43 +0300

    refactor: removae changes for main page 31   32

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
-    const [value, setValue] = React.useState('');
-
-    const onChange = (value: string) => {
-        setValue(value);
-    };
-
     return (
         <div>
-            <Input value={value} placeholder="введите текст" onChange={onChange} />
             {t('Главная страница')}
         </div>
     );
