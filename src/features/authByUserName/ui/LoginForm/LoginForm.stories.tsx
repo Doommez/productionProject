4d8323dae8a63549616c6e4fce49a24da3a58b0f
E. Boganov <jenyaz862@gmail.com>
2026-02-09 16:53:12 +0300

    feat: add login form stories with error

--- src/features/authByUserName/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/authByUserName/ui/LoginForm/LoginForm.stories.tsx
@@ -5,6 +5,7 @@ import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDeco
 import { Theme } from 'app/providers/themeProvider';
 import { LoginForm } from 'features/authByUserName/ui/LoginForm/LoginForm';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import i18n from 'shared/config/i18n/i18n';
 
 const meta = {
     title: 'feature/LoginForm',
@@ -26,3 +27,12 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+export const WithError: Story = {
+    decorators: [StoreDecorator({
+        loginForm: {
+            username: 'tesz',
+            password: 'tesz',
+            error: i18n.t('Неверный вход')
+        }
+    })]
+};
