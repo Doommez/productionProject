3b456270c3f0b0a6a073f66b3c5b1c3f7c1055e8
e.boganov <jenyaz862@gmail.com>
2026-02-18 09:18:27 +0300

    feat: start create asinc reducer 1752

--- src/app/providers/StoreProvider/config/StateShema.ts
+++ src/app/providers/StoreProvider/config/StateShema.ts
@@ -1,9 +1,25 @@
 import { CounterSchema } from 'entities/Counter';
 import { UserSchema } from 'entities/User';
 import { LoginSchema } from 'features/authByUserName';
+import { EnhancedStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { Reducer, UnknownAction } from 'redux';
 
 export interface StateSchema {
     counter: CounterSchema;
     user: UserSchema;
+    // асинхронный редюсер
     loginForm?: LoginSchema;
+}
+
+export type StateSchemaKey = keyof StateSchema;
+
+export interface ReducerManager {
+    getReducerMap: () => ReducersMapObject<StateSchema>;
+    reduce: (state: StateSchema, action: UnknownAction) => StateSchema;
+    add: (key: StateSchemaKey, reducer: Reducer) => void;
+    remove: (key: StateSchemaKey) => void;
+}
+
+export interface ReduxStoreWithManager extends EnhancedStore {
+    reducerManager: ReducerManager;
 }
